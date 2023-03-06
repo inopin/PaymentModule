0111f31ff11d9bd5aa8ac2f9e00bb76a3c9845ad
Isaev Ivan <valamart9@gmail.com>
2023-03-06 23:31:11 +0300

    panValidationAdd

--- payment-app/src/components/serviceComponents/card/CardComponent.vue
+++ payment-app/src/components/serviceComponents/card/CardComponent.vue
@@ -1,29 +1,47 @@
 <template>
   <section>
     <div class="input__wrapper pan">
-      <input type="tel" placeholder="pan" name="pan">
-      <div class="input-error">errorr message</div>
+      <input  type="tel" 
+              placeholder="pan" 
+              name="pan" 
+              required 
+              v-model="pan" 
+              @input="inputDigits"
+              @blur="checkValidation"
+              maxlength="19">
+      <div class="input-error" v-if="panErrorMessage.length">{{panErrorMessage}}</div>
     </div>
     <div class="input__wrapper date">
-      <input type="tel" placeholder="date" name="date">
+      <input type="tel" placeholder="date" name="date" required v-model="date">
       <div class="input-error">errorr message</div>
     </div>
     <div class="input__wrapper cvv">
-      <input type="tel" placeholder="cvv" name="cvv">
+      <input type="tel" placeholder="cvv" name="cvv" v-model="cvv">
       <div class="input-error">errorr message</div>
     </div>
   </section>
 </template>
 
 <script>
-// import InputPage from '../input/InputPage.vue';
 export default {
-  // template: {
-  //   InputPage
-  // },
   data() {
     return {
-
+      panErrorMessage:'',
+      pan: null,
+      date: null,
+      cvv:null,
+    }
+  },
+  methods: {
+    inputDigits() {
+      this.pan = this.pan.replace(/[^\d]/g, '').replace(/\B(?=(?:\d{4})+(?!\d))/g, ' ')
+    },
+    checkValidation() {
+      if(!this.pan) {
+        this.panErrorMessage = 'введите номер карты'
+      } else if(this.pan.length < 19) {
+        this.panErrorMessage ='пожалуйста, введите карту полностью'
+      }
     }
   }
 }
