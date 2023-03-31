876de516e0662d2bd724ee8100a8648b10ee2de8
ivanisaev <ivan.isaev@best2pay.net>
2023-03-31 18:15:07 +0300

    fix

--- payment-app/src/components/serviceComponents/card/CardComponent.vue
+++ payment-app/src/components/serviceComponents/card/CardComponent.vue
@@ -51,7 +51,10 @@ import {formatPan, inputDigits} from './../../../utils.js'
 export default {
   data() {
     return {
-      paymentObject: {
+      panBlur:false,
+      dateBlur:false,
+      cvvBlur:false,
+      paymentObj: {
         pan:null,
         date:null,
         cvv:null
@@ -59,39 +62,118 @@ export default {
     }
   },
   watch:{
-
+    paymentObj: {
+      handler() {
+        let panMessage = this.panErrorMessage,
+            dateMessage = this.dateErrorMessage,
+            cvvMessage = this.cvvMessageError
+        console.log(panMessage && this.panBlur && dateMessage && this.dateBlur && cvvMessage &&)
+        if(errorMessage === '' && this.panBlur) {
+          alert('true')
+        }
+      },
+      deep: true
+    }
   },
   computed:{
+    panErrorMessage() {
+      let errorMessagePan
+      if(!this.paymentObj.pan) {
+        errorMessagePan = 'введите номер карты'
+      } else if (this.paymentObj.pan.length < 19) {
+        errorMessagePan = 'пожалуйста, введите карту полностью'
+      } else {
+        errorMessagePan = ''
+      }
+      return this.panBlur? errorMessagePan: ''
+    },
 
+    dateErrorMessage() {
+      let isValid = null
+      if(this.paymentObj.date) {
+        let splitted = this.paymentObj.date.replace(/\s/, '').split('/'), isValidDate,
+          isMonthValid = false,
+          isYearValid = false
 
-    // dateErrorMessage() {
-    //   let isValid = null
-    //   if(this.paymentObj.date) {
-    //     let splitted = this.paymentObj.date.replace(/\s/, '').split('/'), isValidDate,
-    //       isMonthValid = false,
-    //       isYearValid = false
+      isMonthValid = parseInt(splitted[1]) === 22 ?
+          parseInt(splitted[0]) >= 3 && parseInt(splitted[0]) <= 12 :
+          parseInt(splitted[0]) > 0 && parseInt(splitted[0]) <= 12
 
-    //   isMonthValid = parseInt(splitted[1]) === 22 ?
-    //       parseInt(splitted[0]) >= 3 && parseInt(splitted[0]) <= 12 :
-    //       parseInt(splitted[0]) > 0 && parseInt(splitted[0]) <= 12
+      isYearValid = parseInt(splitted[1]) >= 22
+      isValidDate = isMonthValid && isYearValid
+      isValid = this.paymentObj.date.length >= 5 && isValidDate
+      }
 
-    //   isYearValid = parseInt(splitted[1]) >= 22
-    //   isValidDate = isMonthValid && isYearValid
-    //   isValid = this.paymentObj.date.length >= 5 && isValidDate
-    //   }
+      if(!isValid && this.dateBlur) {
+        return 'введите коррекную дату'
+      } else {
+        return ''
+      }
+    },
 
-    //   if(!isValid && this.dateBlur) {
-    //     return 'введите коррекную дату'
-    //   } else {
-    //     return ''
-    //   }
-    // },
-
-
+    cvvMessageError() {
+      let errorMessageCvc
+      if(!this.paymentObj.cvv) {
+        errorMessageCvc = 'введите код карты'
+      } else if (this.paymentObj.cvv.length < 3) {
+        errorMessageCvc = 'введите полный код карты'
+      } else {
+        errorMessageCvc = ''
+      }
+      return this.cvvBlur?  errorMessageCvc : ''
+    }
   },
   methods: {
+    panInput() {
+      // console.log(Object.keys(this.paymentObj))
+      this.paymentObj.pan = inputDigits(this.paymentObj.pan)
+      this.paymentObj.pan = formatPan(this.paymentObj.pan)
+      this.panBlur = false
+    },
+
+    isPanBlur() {
+      this.panBlur = true
+    },
+
+    isDateBlur() {
+      this.dateBlur = true
+    },
+
+    isCvvBlur() {
+      this.cvvBlur = true
+    },
+
+    dateInput(e) {
+      if (e.inputType === 'deleteContentBackward') return
+      this.dateErrorMessage = ''
+      this.paymentObj.date = inputDigits(this.paymentObj.date)
+      this.paymentObj.date = this.paymentObj.date.length > 4 ? this.paymentObj.date.substring(0, 2) + this.paymentObj.date.substring(4, 6) : this.paymentObj.date.substring(0, 4);
+
+      if(this.paymentObj.date.length >= 3) {
+        this.paymentObj.date = this.paymentObj.date.substring(0, 2) + ' / ' + this.paymentObj.date.substring(2, 4)
+      }
+      if(this.paymentObj.date.length === 2) {
+        this.paymentObj.date = this.paymentObj.date + ' / '
+      }
+      if(this.paymentObj.date.length > 2) {
+        if(parseInt(this.paymentObj.date.split(' / ')[0]) === 0) {
+          this.paymentObj.date = '01' + this.paymentObj.date.substring(2, 4)
+        }
+
+        if(parseInt(this.paymentObj.date.split(' / ')[0]) > 12) {
+          this.paymentObj.date = 12 + this.paymentObj.date.substring(2, 4)
+        }
+      }
+      this.dateBlur = false
+    },
+
+    cvvInput() {
+      this.paymentObj.cvv = inputDigits(this.paymentObj.cvv)
+      this.cvvBlur = false
+    },
 
   },
+  emits: ['pan-submit','date-submit','cvv-submit']
 }
 </script>
 
