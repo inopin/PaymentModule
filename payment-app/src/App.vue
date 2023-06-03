3c47b6101ad1ef63cb0d9a8169edf823aa5e2dd4
Isaev Ivan <valamart9@gmail.com>
2023-06-03 16:43:46 +0300

    stage1

--- payment-app/src/App.vue
+++ payment-app/src/App.vue
@@ -9,6 +9,20 @@ export default {
   name: 'App',
   components: {
     PaymentPage
+  },
+  data() {
+    return {
+      
+    }
+  },
+  provide: {
+    options: [
+        {id:1, bankName:'bank1'},
+        {id:2, bankName:'bank2'},
+        {id:3, bankName:'bank3'},
+        {id:4, bankName:'bank4'},
+        {id:5, bankName:'bank5'},
+      ]
   }
 }
 </script>
