86d928cc4d3e83a288a319faa88c1480bcf5459b
Isaev Ivan <valamart9@gmail.com>
2023-06-04 18:45:48 +0300

    filterAdd

--- payment-app/src/components/serviceComponents/select/SelectComponent.vue
+++ payment-app/src/components/serviceComponents/select/SelectComponent.vue
@@ -3,8 +3,11 @@
     <input type="hidden" v-model="this.selectedOption">
     <p class="title" @click="this.listVisible = !this.listVisible">{{ this.selectedOption }}</p>
     <div class="options" v-if="this.listVisible">
-      <div v-for="item in options"
-        :key="item.id"
+      <div class="search">
+        <input type="text" placeholder="выберите банк" v-model="searchValue">
+      </div>
+      <div v-for="item in searchArray"
+        :key="item.index"
         class="item"
         @click="selectItem(item)">
         <p>{{ item.bankName }}</p>
@@ -30,18 +33,35 @@
       },
       hideSelect() {
         this.listVisible= false
-      }
+      },
+      search(val, arr) {
+        let result = []
+        arr.forEach(item =>{
+          if(item.bankName.indexOf(val)!=-1) {
+            result.push(item)
+          }
+        })
+        this.searchArray = result
+      },
+    },
+    watch:{
+        searchValue: function() {
+          this.searchArray = this.options
+          this.search(this.searchValue, this.searchArray)
+        }
     },
     mounted(){
-       document.addEventListener('click', this.hideSelect.bind(this), true)
+      //  document.addEventListener('click', this.hideSelect.bind(this), true)
     },
     beforeUnmount(){
-      document.removeEventListener('click', this.hideSelect.bind(this), true)
+      // document.removeEventListener('click', this.hideSelect.bind(this), true)
     },
     data() {
       return {
         listVisible: false,
-        selectedOption: 'выберите банк'
+        selectedOption: 'выберите банк',
+        searchValue:'',
+        searchArray : this.options
       }
     }
   }
